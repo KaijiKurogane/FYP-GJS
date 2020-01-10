6e8f8239fe3026e9264b8e5265e6e5e7cc46176d
KaijiKurogane <47972838+KaijiKurogane@users.noreply.github.com>
2020-01-11 01:35:19 +0800

    Update firebase.js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,14 +3,14 @@ import "firebase/auth";
 import "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB78r6uZ9UQfbu5-debTZbOS8dbdIod9u4",
-  authDomain: "gombak-jungle-school-6be9a.firebaseapp.com",
-  databaseURL: "https://gombak-jungle-school-6be9a.firebaseio.com",
-  projectId: "gombak-jungle-school-6be9a",
-  storageBucket: "gombak-jungle-school-6be9a.appspot.com",
-  messagingSenderId: "758117696829",
-  appId: "1:758117696829:web:da502895ade86bec0cb154",
-  measurementId: "G-16MXNZZVPX"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 export const myFirebase = firebase.initializeApp(firebaseConfig);
