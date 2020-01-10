4f15331421a185588a0e73bf63002931408ebab9
KaijiKurogane <47972838+KaijiKurogane@users.noreply.github.com>
2020-01-11 01:34:08 +0800

    first commit

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,14 @@
 import Firebase from 'firebase'
   
 const config = {
-  apiKey: "AIzaSyB78r6uZ9UQfbu5-debTZbOS8dbdIod9u4",
-  authDomain: "gombak-jungle-school-6be9a.firebaseapp.com",
-  databaseURL: "https://gombak-jungle-school-6be9a.firebaseio.com",
-  projectId: "gombak-jungle-school-6be9a",
-  storageBucket: "gombak-jungle-school-6be9a.appspot.com",
-  messagingSenderId: "758117696829",
-  appId: "1:758117696829:web:da502895ade86bec0cb154",
-  measurementId: "G-16MXNZZVPX"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 const database = Firebase.initializeApp(config)
 const firebase = database.database()
